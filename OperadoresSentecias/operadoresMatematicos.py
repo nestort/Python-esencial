8f4d93fbe73ed4594b9817d4c36963016cdaa9d1
nestort <nestrimo@gmail.com>
2020-12-23 01:45:12 -0600

    operadores matematicos

---
+++ OperadoresSentecias/operadoresMatematicos.py
@@ -0,0 +1,25 @@
+x=11
+y=10
+
+#Suma
+suma=x+y
+print(suma)
+#Resta
+resta=x-y
+print(resta)
+#Multiplicacion
+multiplicacion=x*y
+print(multiplicacion)
+#Division redondeado con decimal
+divisionA=x/y
+print(divisionA)
+#Division redondeado 
+divisionB=x//y
+print(divisionB)
+#Residuo division
+residuo=x%y
+print(residuo)
+#Potencia ej x^2
+potencia=x**2
+print(potencia)
+
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## Presentación del curso e introducción a Python
 ### [Variables en Python](./Presentacion/variables.py)
 
+## Operadores y sentencias de control en Python
+### [Operadores matemáticos](./OperadoresSentecias/operadoresMatematicos.py)
 
 
 
