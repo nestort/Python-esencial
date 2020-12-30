dc596e2bdb0b30c840927d32425c65ea55338b1e
nestort <nestrimo@gmail.com>
2020-12-30 12:42:08 -0600

    uso de funciones

--- UsoDeFunciones/funcion.py
+++ UsoDeFunciones/funcion.py
@@ -1,14 +1,29 @@
-#Funciones
+#Funcion simple
+def comer():
+    print("Comiendo...")
+
+#Funcion recibe un parametro 
 def saludo(usuario):
     print("Hola,",usuario)
-def comer():
-    print("Comiendo")
 
-#Uso de du funciones
-nombreUsuario="Bossie"
+#Funcion retorna un valor 
+def getEdad():
+    return 105;
 
-saludo(nombreUsuario)
+#Uso de funcion simple 
 comer()
+
+#Uso de funcion que recibe parametro
+nombre="Obi wan"
+saludo(nombre)
+
+#Uso de funcion que retorna valor 
+edad=getEdad()
+print("la edad es",edad)
+
+
+
+
 
 
 
