fea8e6b45b3016ff44fd83c949df60ea66b1ebb1
nestort <nestrimo@gmail.com>
2020-12-30 13:33:12 -0600

    refactorizacion

--- UsoDeFunciones/funcion.py
+++ UsoDeFunciones/funcion.py
@@ -1,29 +1,34 @@
-#Funcion simple
-def comer():
+# Funcion simple
+def eat():
     print("Comiendo...")
 
-#Funcion recibe un parametro 
-def saludo(usuario):
-    print("Hola,",usuario)
+# Funcion recibe un parametro
+def greet(person):
+    print("Hola,", person)
 
-#Funcion retorna un valor 
-def getEdad():
-    return 105;
+# Funcion retorna un valor
+def getAge():
+    return 105
 
-#Uso de funcion simple 
-comer()
-
-#Uso de funcion que recibe parametro
-nombre="Obi wan"
-saludo(nombre)
-
-#Uso de funcion que retorna valor 
-edad=getEdad()
-print("la edad es",edad)
+# Uso de funcion con parametro opcional y requerido
+# El parametro 'a' es requerido mientras que el 'b' es opcional
+def sumOfAge(a, b=0):
+    return a+b
 
 
+# Uso de funcion simple
+eat()
 
+# Uso de funcion que recibe parametro
+person = "Obi wan"
+greet(person)
 
+# Uso de funcion que retorna valor
+age = getAge()
+print("la edad es", age)
 
-
-
+# Uso de funcion con parametro opcional
+test1 = sumOfAge(1)
+test2 = sumOfAge(3,2)
+print("Se paso el valor 'a' a la función, Resultado: ", test1)
+print("Se paso el valor 'a' y 'b' a la función, Resultado: ", test2)
